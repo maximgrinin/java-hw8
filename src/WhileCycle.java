2d28a631f771260abf7b50a6a1721daec64fe852
maximgrinin <maximgrinin@gmail.com>
2022-11-22 09:05:49 +0300

    Class-examples added

---
+++ src/WhileCycle.java
@@ -0,0 +1,16 @@
+
+public class WhileCycle {
+    public static void main(String[] args) {
+
+        int daysToDie = 365 * 75;
+        while (daysToDie != 0) {
+            System.out.println("Проснулся. Потянулся! " + daysToDie);
+            daysToDie--;
+        }
+        System.out.println("До встречи!");
+
+        //циклы, которые выполняются ограниченное число раз
+
+
+    }
+}
---
+++ src/WhileSecondCycle.java
@@ -0,0 +1,69 @@
+
+public class WhileSecondCycle {
+    public static void main(String[] args) throws InterruptedException {
+        //вывести все числа от 1 до 1000, которые кончаются на 0
+        //int a = 0;
+        //while (a < 1000) {
+        //    a += 10;
+        //    System.out.println(a);
+        //}
+
+
+
+        //вывести все степени двойки
+        //int number = 1;
+        //while (number > 0) {
+        //    number *= 2;
+        //    System.out.println(number);
+        //    Thread.sleep(1000);
+        //}
+
+        //1
+        //2
+        //4
+        //8
+        //..-2147000000
+
+        //999
+        //1
+        //900
+        //1000
+
+        //просуммировать все введеные с консоли значения
+        //Scanner scanner = new Scanner(System.in);
+        //int x = scanner.nextInt();
+        //int sum = 0;
+
+        //while (x > 0) {
+        //    int a = scanner.nextInt();
+        //    sum += a;
+        //    x--;
+        //}
+
+        //System.out.println(sum);
+
+        //просто вложенный цикл
+        int b = 0;
+        int a = 0;
+        while (a < 10) {
+            a++;
+            System.out.println("a=" + a);
+            while (b < 10) {
+                b++;
+                System.out.println("b=" + b);
+            }
+        }
+
+        //Написать такие числа на экран 2, 4, 6, 8 .... 996, 998, 1000
+        //Написать на экране все числа от 0 до 1000, которые кончаются на 7: 7, 17, 27
+
+        //20-40 встречаемся
+
+
+
+
+        //область видимости
+
+
+    }
+}
